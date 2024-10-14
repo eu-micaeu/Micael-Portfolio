578481e3244d76984fe5d01062644841fdb60951
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2024-10-14 03:59:29 -0300

    Refactor CSS styles and update content in App.css and App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
 
         <div className="part1">
 
-          <h1>Mica</h1>
+          <h1>M</h1>
 
           <p>I am currently expanding my expertise in Software Engineering at UTFPR, with completion scheduled for December 2025. At Tata Consultancy Services, as a Junior Systems Analyst, I applied my knowledge on a daily basis in protocols such as HTTP and design tools such as Figma, in addition to manage MySQL databases, which consolidated my technical skills.
 
@@ -228,6 +228,16 @@ function App() {
 
         </div>
 
+        <div className="frameworks-item">
+
+          <svg className="link" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+
+          <img src="./angular.png" width={100} height={100}></img>
+
+          <h1>Angular</h1>
+
+        </div>
+
       </div>
 
       <div className="nameTechnologies">
@@ -271,6 +281,16 @@ function App() {
           <img src="./visual.png" width={100} height={100}></img>
 
           <h1>Visual Studio Code</h1>
+
+        </div>
+
+        <div className="technologies-item">
+
+          <svg className="link" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g> </g></svg>
+
+          <img src="./figma.png" width={100} height={100}></img>
+
+          <h1>Figma</h1>
 
         </div>
 
